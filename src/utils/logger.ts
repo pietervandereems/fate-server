1dc351c874c29c031f524ac34d6717322afd0017
Pieter van der Eems <gpggit@eemco.nl>
2021-05-16 16:50:47 +0200

    Simple logger lib allowing for loglevel setting

---
+++ src/utils/logger.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express'
+import { LogLevel } from '../models/error.model'
+
+const logError = (...args: any): void => {
+  console.error(new Date(), ...args)
+}
+
+const debug = (...args: any): void => {
+  if (process.env.LOG_LEVEL === LogLevel.DEBUG) {
+    console.debug(new Date(), ...args)
+  }
+}
+
+const info = (...args: any): void => {
+  if (process.env.LOG_LEVEL === LogLevel.DEBUG || process.env.LOG_LEVEL === LogLevel.INFO) {
+    console.info(new Date(), ...args)
+  }
+}
+
+const network = (req: Request, _res: Response, next: NextFunction): void => {
+  debug('Request', req)
+  next()
+}
+
+export {
+  logError,
+  debug,
+  info,
+  network
+}
