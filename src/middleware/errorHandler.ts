a578b21bd91b1d7693386a97dba1d29b140bb14d
Pieter van der Eems <gpggit@eemco.nl>
2021-05-16 16:51:10 +0200

    Default error handler to catch express router errors mainly

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,59 @@
+import { Request, Response, NextFunction } from 'express'
+import { logError } from '../utils/logger'
+import { ErrorStatuses, requestError, RFC7807 } from '../models/error.model'
+
+const errorHandler = (err: requestError, _request: Request, response: Response, _next: NextFunction): any => {
+  const respond = (message: RFC7807): any => response.status(message.status).send(message)
+
+  logError('An error has been caught by the errorHandler, err:', err)
+
+  const msg = {
+    type: 'about:blank',
+    detail: err?.message ?? '',
+    instance: err?.path ?? '',
+    errors: (Array.isArray(err?.errors)) ? [...err.errors] : []
+  }
+
+  if (err.isAxiosError) {
+    return respond({
+      ...msg,
+      title: 'Request to MDG failed',
+      status: err.response?.status ?? 400
+    })
+  }
+
+  switch (err.name) {
+    case 'Bad Request':
+      return respond({
+        ...msg,
+        title: 'Bad Request',
+        status: ErrorStatuses['Bad Request']
+      })
+    case 'Bad Gateway':
+      return respond({
+        ...msg,
+        title: 'Bad Gateway',
+        status: ErrorStatuses['Bad Gateway']
+      })
+    case 'Not Found':
+      return respond({
+        ...msg,
+        title: err.name,
+        status: ErrorStatuses['Not Found']
+      })
+    case 'noAuthInfo':
+      return respond({
+        ...msg,
+        title: 'No AuthInfo',
+        status: ErrorStatuses.noAuthInfo
+      })
+    default:
+      return respond({
+        ...msg,
+        title: 'General Error',
+        status: ErrorStatuses['General Error']
+      })
+  }
+}
+
+export default errorHandler
