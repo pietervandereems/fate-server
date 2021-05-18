05fb89d85106ca6b0588da8c4d19c4b876a32cba
Pieter van der Eems <gpggit@eemco.nl>
2021-05-18 19:23:55 +0200

    Fix send

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -2,7 +2,7 @@ import express from 'express'
 const gameRouter = express.Router()
 
 gameRouter.get('/subscribe', (_req: express.Request, res: express.Response) => {
-  res.sendStatus(200).send('yes')
+  res.status(200).send('yes')
 })
 
 gameRouter.get('/ping', (_req: express.Request, res: express.Response) => {
