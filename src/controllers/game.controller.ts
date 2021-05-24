cccf5c027addc3a752d14924d94eb300564d5496
Pieter van der Eems <gpggit@eemco.nl>
2021-05-24 16:40:38 +0200

    Subscribe route first step, get information of the game

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -1,9 +1,26 @@
 import express from 'express'
+import { GameId } from '../models/documents.model'
+import { asyncMiddleware } from '../middleware/asyncMiddelware'
+import { isGameId } from '../utils/typeGuards'
+import { createError } from '../utils/error'
+import { getGame } from '../clients/couchdb.client'
+import { debug } from '../utils/logger'
 const gameRouter = express.Router()
 
-gameRouter.get('/subscribe', (_req: express.Request, res: express.Response) => {
-  res.status(200).send('yes')
-})
+gameRouter.post('/subscribe/:id', asyncMiddleware(async (req: express.Request, res: express.Response) => {
+  const gameId: GameId = req.params.id
+  if (!isGameId(gameId)) {
+    throw createError({
+      name: 'Bad Request',
+      path: req.path,
+      errors: { message: 'The supplied id is not the id of a game' }
+    })
+  }
+
+  const gameDoc = await getGame(gameId)
+  debug({ gameDoc })
+  res.status(200).send(gameDoc)
+}))
 
 gameRouter.get('/ping', (_req: express.Request, res: express.Response) => {
   res.status(200).send('pong')
