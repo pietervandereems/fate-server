ce5f27b18205c458fb3f1e1cc764d5dddf4fd35e
Pieter van der Eems <gpggit@eemco.nl>
2021-05-24 16:39:37 +0200

    Models for the different couchdb documents

---
+++ src/models/documents.model.ts
@@ -0,0 +1,49 @@
+const enum SelectorTypes {
+  character = 'character',
+  game = 'game',
+  player = 'player'
+}
+
+interface ReplicatorDocument {
+  _id: string
+  _rev?: string
+  source: string
+  target: string
+  selector: {
+    type: SelectorTypes
+    player: {
+      $in: string[]
+    }
+  }
+  user_ctx: {
+    name: 'admin'
+    roles: [
+      '_admin',
+      '_reader',
+      '_writer'
+    ]
+  }
+  owner: 'admin'
+  create_target: false
+  continuous: true
+}
+
+interface GameDocument {
+  _id: string
+  _rev: string
+  type: SelectorTypes.game
+  name: string
+  players: string[]
+}
+
+type Documents = GameDocument | ReplicatorDocument
+
+type GameId = string
+
+export {
+  ReplicatorDocument,
+  GameDocument,
+  Documents,
+  GameId,
+  SelectorTypes
+}
