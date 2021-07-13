a37f895560150dc4ba98cb28a4e8edf018095943
Pieter van der Eems <gpggit@eemco.nl>
2021-07-13 20:43:08 +0200

    Add format to validator, add user route

--- src/app.ts
+++ src/app.ts
@@ -6,26 +6,34 @@ import * as OpenApiValidator from 'express-openapi-validator'
 import { network } from './utils/logger'
 import gameRouter from './controllers/game.controller'
 import { checkToken } from './middleware/authorization'
+import userRouter from './controllers/user.controller'
 
 const app = express()
 // patchRouterParam()
-
-app.use(express.json())
+app.use(express.text({ type: 'text/plain' }))
+app.use(express.json({ type: 'application/json' }))
 app.use(network)
-app.use(checkToken)
+app.use(OpenApiValidator.middleware({
+  apiSpec: './openapi.yaml',
+  validateRequests: true,
+  validateResponses: true,
+  formats: [
+    {
+      name: 'username',
+      type: 'string',
+      validate: (value: string) => (value.length > 8)
+    }]
+}))
 
 // -- unprotected handlers go here
 // -- END unprotected handlers
 
-app.use(OpenApiValidator.middleware({
-  apiSpec: './openapi.yaml',
-  validateRequests: true,
-  validateResponses: true
-}))
+app.use(checkToken)
 
 // -- protected handlers go here
 app.use('/web/spec', express.static('./openapi.yaml'))
 app.use('/v1/games', gameRouter)
+app.use('/v1/users', userRouter)
 
 // -- END protected handlers
 
